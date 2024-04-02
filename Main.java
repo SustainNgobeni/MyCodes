daf3201e2af517f29109108112efa68a3cbadff1
SustainNgobeni <145780761+SustainNgobeni@users.noreply.github.com>
2024-04-02 10:18:52 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 
-class Main{
-
-    static void asterisk(){
+public class Main {
+    
+ /*
+   static void asterisk(){
         for(int i=0; i<55; i++){
             System.out.print("*");
         }
         System.out.println();
     }
-    
+ */ 
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
 
         String name;
         int num;
-
+        
         //adding books to library1 arraylist
         Library.library1Books.add("scoa021");
         Library.library1Books.add("scoa022");
@@ -34,19 +35,37 @@ class Main{
 
         System.out.println("Welcome to Mountain Caves Library\n");
         System.out.println("List of the available books in our 2 libraries");
-        asterisk();
+        //asterisk();
         Library.printAvailableBooks1();
         System.out.println("\n");
         Library.printAvailableBooks2();
         System.out.println();
-        asterisk();
+        //asterisk();
         System.out.println();
         
         Book.bookTitle();
         name = input.nextLine();
         
 
-        System.out.println("Enter 1 to borrow book");
-        System.out.println("Enter 2 to borrow book");
+        System.out.println("Enter 1 to borrow book from library1");
+        System.out.println("Enter 2 to return book to library1");
+        System.out.println("Enter 3 to borrow book from library2");
+        System.out.println("Enter 4 to return book to library1");
+        num = input.nextInt();
+
+        if( num == 1){
+            Library.borrowBook1(name , 1);
+            
+        }
+        else if( num == 2){
+            Library.returnBook();
+        }
+        if( num == 3){
+            Library.borrowBook2(name, 1);
+        }
+        if( num == 4){
+            Library.returnBook();  
+        }
     }
+
 }

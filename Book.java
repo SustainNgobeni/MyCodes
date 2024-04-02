5dd810d5fb03b9ccfe30d7db0be0271d73275a9d
SustainNgobeni <145780761+SustainNgobeni@users.noreply.github.com>
2024-04-02 10:20:36 +0200

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,23 +1,14 @@
-class Book{
-
+public class Book {
+    
     static void bookTitle(){
         System.out.println("Enter book name:");
-    }
-
-    static void isBookAvailable(String book, String name){
-        if(name.equals(book)){
-            System.out.println("Book is available.");
-        }
-        else{
-            System.out.println("Book not available.");
-        }
     }
     
     static void canBorrowBook1(String book, String name){
         if (book.equals(name)) {
             System.out.println("Book has been successfully borrowed from library 1");
         } 
-        else if(book.equals(name)) {
+        else if(!book.equals(name)){
             System.out.println("Book is not available.");
         }
     }
@@ -26,16 +17,12 @@ class Book{
         if (book.equals(name)) {
             System.out.println("Book has been successfully borrowed from library 2");
         } 
-        else if(book.equals(name)) {
-            System.out.println("Book is not available.");
-        }
     }
+    
     static void canReturnBook(String book, String name){
         if(book.equals(name)){
-            System.out.println("Can return book.");
-        }
-        else{
-            System.out.println("Cannot return book.");
+            System.out.println("Book successfully returned.");
         }
     }
+
 }

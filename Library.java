ad87e0a7ea21d97982fa20e219fcc9681010661e
SustainNgobeni <145780761+SustainNgobeni@users.noreply.github.com>
2024-04-02 10:20:57 +0200

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 
-class Library{
-
+public class Library {
+    
+  
      static ArrayList<String> library1Books = new ArrayList<String>();
      static ArrayList<String> library2Books = new ArrayList<String>();
+     int x=1;
 
      static void printAvailableBooks1(){
         System.out.println("Books available in Library 1:");
@@ -19,20 +21,32 @@ class Library{
         }
      }
 
-     static void borrowBook1(String book){
+     static void borrowBook1(String book , int x){
       for(int i=0; i<library1Books.size();i++){
+        if(book.equals(library1Books.get(i))){
              Book.canBorrowBook1(book , library1Books.get(i));
+             x--;
+        }
+     }
+     if(x == 1){
+        System.out.print("Book cannot be borrowed");
      }
  }
 
-     static void borrowBook2(String book){
+     static void borrowBook2(String book , int x){
       for(int j=0; j<library2Books.size();j++){
-             Book.canBorrowBook2(book , library2Books.get(j));     
+             if(book.equals(library2Books.get(j))){
+                Book.canBorrowBook2(book , library2Books.get(j));
+                x--;
+        }
+     }
+     if(x == 1){
+        System.out.print("Book cannot be borrowed");
      }
  }
 
      static void returnBook(){
-        
+        System.out.print("Book successfully returned.");
      }
-   
+
 }

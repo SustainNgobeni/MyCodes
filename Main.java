e702051db382f4e2515f28a1f90b5ec858a008ca
SustainNgobeni <145780761+SustainNgobeni@users.noreply.github.com>
2024-04-03 01:58:05 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     
- /*
    static void asterisk(){
         for(int i=0; i<55; i++){
             System.out.print("*");
         }
         System.out.println();
     }
- */ 
+  
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
@@ -18,54 +17,57 @@ public class Main {
         int num;
         
         //adding books to library1 arraylist
-        Library.library1Books.add("scoa021");
-        Library.library1Books.add("scoa022");
-        Library.library1Books.add("ssta021");
-        Library.library1Books.add("ssta0022");
-        Library.library1Books.add("smta021");
-        Library.library1Books.add("smta022");
+        Library.library1Books.add("scoabook");
+        Library.library1Books.add("smtabook");
+        Library.library1Books.add("sstabook");
 
         //adding books to library2 arraylist
-        Library.library2Books.add("scsc011");
-        Library.library2Books.add("scsc012");
-        Library.library2Books.add("ssts011");
-        Library.library2Books.add("ssts012");
-        Library.library2Books.add("smth011");
-        Library.library2Books.add("smth012");
+        Library.library2Books.add("scscbook");
+        Library.library2Books.add("scscbook");
+        Library.library2Books.add("sstsbook");
 
         System.out.println("Welcome to Mountain Caves Library\n");
         System.out.println("List of the available books in our 2 libraries");
-        //asterisk();
+        asterisk();
         Library.printAvailableBooks1();
         System.out.println("\n");
         Library.printAvailableBooks2();
         System.out.println();
-        //asterisk();
+        asterisk();
         System.out.println();
         
         Book.bookTitle();
-        name = input.nextLine();
-        
+        while(!(input.hasNext("[A-Za-z]*"))) {
+            System.out.print("enter correct name!!!!");
+            input.next();
+        }
+        name = input.next();
+        System.out.println();
 
         System.out.println("Enter 1 to borrow book from library1");
         System.out.println("Enter 2 to return book to library1");
         System.out.println("Enter 3 to borrow book from library2");
-        System.out.println("Enter 4 to return book to library1");
+        System.out.println("Enter 4 to return book to library2");
+        while(!(input.hasNextInt())) {
+            System.out.print("enter correct number!!!!");
+            input.next();
+        }
         num = input.nextInt();
 
         if( num == 1){
-            Library.borrowBook1(name , 1);
-            
+            Library.borrowBook1(name , 1);    
         }
         else if( num == 2){
-            Library.returnBook();
+            Library.returnBook1(name, 1);
         }
-        if( num == 3){
+        else if( num == 3){
             Library.borrowBook2(name, 1);
         }
-        if( num == 4){
-            Library.returnBook();  
+        else if( num == 4){
+            Library.returnBook2(name, 1);  
+        }
+        else{
+            System.out.println("Wrong number chosen!!!! Try again");
         }
     }
-
 }

536f11a7cc306d7f6af2d158a5f39cb725cb1777
SustainNgobeni <145780761+SustainNgobeni@users.noreply.github.com>
2024-04-15 07:08:05 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,157 +1,80 @@
 import java.util.Scanner;
 
 public class Main {
-    
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
+
+        //declare variables
+        String name;
         int num;
-
-        Library library1 = new Library("Library1");
-        Library library2 = new Library("Library2");
-
-        library1.addBook("scoa");
-        library1.addBook("ssta");
-        library1.addBook("smta");
-
-        library2.addBook("scsc");
-        library2.addBook("ssts");
-        library2.addBook("smth");
-
-        System.out.println("Books available in " + library1.getlibraryName() + ":");
-        library1.printAvailableBooks();
-        System.out.println();
-        System.out.println("Books available in " + library1.getlibraryName() + ":");
-        library2.printAvailableBooks();
-
-        System.out.println("\nEnter 1 to borrow book from library1");
-        System.out.println("Enter 2 to return book to library1");
-        System.out.println("Enter 3 to borrow book from library2");
-        System.out.println("Enter 4 to return book to library2");
-        while(!(input.hasNextInt())) {
-            System.out.print("Enter correct number!!!!");
-            input.next();
-        }
-        num = input.nextInt();
-
-        if( num == 1){
-        }
-        else if( num == 2){
-
-        }
-        else if( num == 3){
-    
-        }
-        else if( num == 4){
-
-        }
-        else{
-            System.out.println("Wrong number chosen!!!! Try again");
-        }
-    }
-}
-        import java.util.Scanner;
-
-public class Main {
-    
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-        int num;
-
-        Library library1 = new Library("Library1");
-        Library library2 = new Library("Library2");
-
-        library1.addBook("scoa");
-        library1.addBook("ssta");
-        library1.addBook("smta");
-
-        library2.addBook("scsc");
-        library2.addBook("ssts");
-        library2.addBook("smth");
-
-        System.out.println("Books available in " + library1.getlibraryName() + ":");
-        library1.printAvailableBooks();
-        System.out.println();
-        System.out.println("Books available in " + library1.getlibraryName() + ":");
-        library2.printAvailableBooks();
-
-        System.out.println("\nEnter 1 to borrow book from library1");
-        System.out.println("Enter 2 to return book to library1");
-        System.out.println("Enter 3 to borrow book from library2");
-        System.out.println("Enter 4 to return book to library2");
-        while(!(input.hasNextInt())) {
-            System.out.print("Enter correct number!!!!");
-            input.next();
-        }
-        num = input.nextInt();
-
-        if( num == 1){
-        }
-        else if( num == 2){
-
-        }
-        else if( num == 3){
-    
-        }
-        else if( num == 4){
-
-        }
-        else{
-            System.out.println("Wrong number chosen!!!! Try again");
-        }
-    }
-}
-        import java.util.Scanner;
-
-public class Main {
-    
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-        int num;
-
-        Library library1 = new Library("Library1");
-        Library library2 = new Library("Library2");
-
-        library1.addBook("scoa");
-        library1.addBook("ssta");
-        library1.addBook("smta");
-
-        library2.addBook("scsc");
-        library2.addBook("ssts");
-        library2.addBook("smth");
-
-        System.out.println("Books available in " + library1.getlibraryName() + ":");
-        library1.printAvailableBooks();
-        System.out.println();
-        System.out.println("Books available in " + library1.getlibraryName() + ":");
-        library2.printAvailableBooks();
-
-        System.out.println("\nEnter 1 to borrow book from library1");
-        System.out.println("Enter 2 to return book to library1");
-        System.out.println("Enter 3 to borrow book from library2");
-        System.out.println("Enter 4 to return book to library2");
-        while(!(input.hasNextInt())) {
-            System.out.print("Enter correct number!!!!");
-            input.next();
-        }
-        num = input.nextInt();
-
-        if( num == 1){
-        }
-        else if( num == 2){
-
-        }
-        else if( num == 3){
-    
-        }
-        else if( num == 4){
-
-        }
-        else{
-            System.out.println("Wrong number chosen!!!! Try again");
-        }
-    }
-}
         
+        //adding books to library1 arraylist
+        Library.library1Books.add("scoabook");
+        Library.library1Books.add("smtabook");
+        Library.library1Books.add("sstabook");
+
+        //adding books to library2 arraylist
+        Library.library2Books.add("scscbook");
+        Library.library2Books.add("smthbook");
+        Library.library2Books.add("sstsbook");
+
+        //Prints available books , welcome message , opening and closing time
+        System.out.println("Welcome to Mountain Caves Library(Open from 9AM - 5Pm daily)\n");
+        System.out.println("List of the available books in our 2 libraries");
+        asterisk();
+        Library.printAvailableBooks1();
+        System.out.println("\n");
+        Library.printAvailableBooks2();
+        System.out.println();
+        asterisk();
+        System.out.println();
+        
+        Book.bookTitle();
+        //Input valid to check if user entered a valid name
+        while(!(input.hasNext("[A-Za-z]*"))) {
+            System.out.print("enter correct name!!!!");
+            input.next();
+        }
+        name = input.next();
+        System.out.println();
+
+        //promts user to choose an option
+        System.out.println("Enter 1 to borrow book from library1");
+        System.out.println("Enter 2 to return book to library1");
+        System.out.println("Enter 3 to borrow book from library2");
+        System.out.println("Enter 4 to return book to library2");
+
+        //Input validation to check if user entered a number
+        while(!(input.hasNextInt())) {
+            System.out.print("enter correct number!!!!");
+            input.next();
+        }
+        num = input.nextInt();
+
+        //switch statement to check option chosen by user
+        switch(num){
+            case 1:
+                Library.borrowBook1(name , 1);
+                break;
+            case 2:
+                Library.returnBook1(name , 1);
+                break;
+            case 3:
+                Library.borrowBook1(name , 1);
+                break;
+            case 4:
+                Library.returnBook1(name , 1);
+                break;    
+            default:
+                System.out.println("Wrong number chosen!!!! Try again");    
+        }
+    }
+
+    //method that prints asterisk
+    static void asterisk(){
+        for(int i=0; i<55; i++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}

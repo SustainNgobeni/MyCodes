6c5602ab72aca19982b626c28527cd58140dcd6e
SustainNgobeni <145780761+SustainNgobeni@users.noreply.github.com>
2024-04-15 07:07:22 +0200

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,37 +1,79 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Library {
-    private String libraryName;
-    private String [] books = new String[3];
+     //Creates arrayList objects. Also declare and initialize x 
+     static ArrayList<String> library1Books = new ArrayList<String>();
+     static ArrayList<String> library2Books = new ArrayList<String>();
+     int x=1;
 
-    public Library(String libraryName){
-        this.libraryName = libraryName;
-    }
-    
-    public String getlibraryName(){
-        return libraryName;
-    }
-
-    public void setlibraryName(String libraryName){
-        this.libraryName = libraryName;
-    }
-
-    public void addBook(String book){
-        for(int i=0; i<books.length; i++){
-           books[i] = book; 
+     //method that sorts and prints library1Books
+     public static void printAvailableBooks1(){
+        System.out.println("Books available in Library 1:");
+        for(int i=0; i<library1Books.size();i++){
+         Collections.sort(library1Books);
+            System.out.print( library1Books.get(i) + " ");
         }
+     }
+
+     //method that sorts and prints library2Books
+     public static void printAvailableBooks2(){
+      System.out.println("Books available in Library 2:");
+        Collections.sort(library2Books);
+        for(int j=0; j<library1Books.size();j++){
+            System.out.print( library2Books.get(j) + " ");
+        }
+     }
+
+     //method to borrow books from library1
+     public static void borrowBook1(String book , int x){
+      for(int i=0; i<library1Books.size();i++){
+        if(book.equals(library1Books.get(i))){
+             Book.canBorrowBook1(book , library1Books.get(i));
+             x--;
+        }
+     }
+     if(x == 1){
+        System.out.print("Book cannot be borrowed");
+      }
+    }
+     //method to borrow books from library2
+     public static void borrowBook2(String book , int x){
+        for(int j=0; j<library2Books.size();j++){
+             if(book.equals(library2Books.get(j))){
+                Book.canBorrowBook2(book , library2Books.get(j));
+                x--;
+               }
+              }
+              if(x == 1){
+              System.out.print("Book cannot be borrowed");
+                }
+      }
+
+     //method to return book to library1
+     public static void returnBook1(String book , int x){
+         for(int p=0; p<library1Books.size();p++){
+             if(book.equals(library1Books.get(p))){
+               Book.canReturnBook(book , library1Books.get(p) );
+               x--;
+              }
+            }
+            if(x == 1){
+            System.out.print("Book cannot be returned");
+            }
     }
 
-    public void printAvailableBooks(){
-        int high = books.length;
-        int low = 0;
-        if(high >= low){
-            for(int i=0; i<books.length; i++){
-                System.out.print(books[i] + " "); 
+    //method to return book to library2
+    public static void returnBook2(String book , int x){
+        for(int a=0; a<library2Books.size();a++){
+             if(book.equals(library2Books.get(a))){
+             Book.canReturnBook(book , library1Books.get(a) );
+             x--;
              }
-        }
-        else{
-            System.out.print("No books available!!!1");
-        }
-    }
- }
+            }
+             if(x == 1){
+             System.out.print("Book cannot be returned");
+         }
+      }
+
+}

3119872cfbfd0be2360274e8310ad43c6c7cec48
SustainNgobeni <145780761+SustainNgobeni@users.noreply.github.com>
2024-04-22 07:10:30 +0200

    Update Book.java

--- Book.java
+++ Book.java
@@ -2,7 +2,7 @@ public class Book {
     
     //method that prompts user to enter book name 
     public static void bookTitle(){
-        System.out.print("Enter book name:");
+        System.out.println("Enter book name:");
     }
     
     //method to check if book is available
@@ -16,21 +16,21 @@ public class Book {
     }
 
     //method to check if you can borrow books from library 1
-    static void canBorrowBook1(String book, String name){
+    public static void canBorrowBook1(String book, String name){
         if (isBookAvailable(book, name) == true) {
             System.out.println("Book has been successfully borrowed from library 1");
         }
     }
 
     //method to check if you can borrow books from library 2
-    static void canBorrowBook2(String book, String name){
+    public static void canBorrowBook2(String book, String name){
         if (isBookAvailable(book, name) == true) {
             System.out.println("Book has been successfully borrowed from library 2");
         } 
     }
     
     //method to check if you can return book
-    static void canReturnBook(String book, String name){
+    public static void canReturnBook(String book, String name){
         if(isBookAvailable(book, name) == true){
             System.out.println("Book successfully returned.");
         }
